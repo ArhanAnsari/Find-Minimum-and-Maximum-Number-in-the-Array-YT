01499bffa92c6f086567b855323e884e9b9d589d
Arhan Ansari <arhanansari2009@gmail.com>
2024-06-25 07:02:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+#Find Minimum and Maximum Number in the array
+
+#Define a Array
+array = [51,62,35,40,57,68,70,87,99,100]
+arr = [5]
+
+#Find the minimum number in the array
+min_num = min(array)
+minnum = min(arr)
+
+#Find the maximum number in the array
+max_num = max(array)
+maxnum = max(arr)
+
+#Print the output
+
+print(f"The minimum number in the array is: {min_num}")
+
+print(f"The maximum number in the array is: {max_num}")
+
+print(f"The minimum number in the arr is: {minnum}")
+
+print(f"The maximum number in the arr is: {maxnum}")
